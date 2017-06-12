39e54ce9c0c551a1180a995f98775b15c918460c
evgeniy.kulikov <evgeniy.kulikov@ainstainer.de>
2017-06-12 14:26:31 +0300

    BeforeTestRun() method was updated

--- AndroidAppTestSuit/hooks/FeatureHooks.cs
+++ AndroidAppTestSuit/hooks/FeatureHooks.cs
@@ -16,10 +16,8 @@ namespace AndroidAppTestSuit.hooks
         public static void BeforeTestRun()
         {
             //            //--------- comment  device name/OS version sections for running from VS
-
             ////            runtests.cmd "Google Nexus 5_4.4" testpath: Feature* Login*
-            ////        "Google Nexus 5_4.4"
-            ////        "Google Nexus 10_5.0"
+
             //////      Choose a device name (ex. "Google Nexus 5", "Google Nexus 10")
             // Environment.SetEnvironmentVariable("env.DeviceNameAndOS", "Google Nexus 5_4.4");
 
@@ -27,9 +25,7 @@ namespace AndroidAppTestSuit.hooks
             Environment.SetEnvironmentVariable("env.AppVersion", "com.soundcloud.android_2017.05.30-beta-674_minAPI16(armeabi,armeabi-v7a,x86)(nodpi)_apkmirror.com.apk");
 
             string deviceNameAndOS = Environment.GetEnvironmentVariable("env.DeviceNameAndOS").Replace("\"", "");
-
-            Console.WriteLine("|| deviceNameAndOS |  " + deviceNameAndOS);
-
+            
             Console.WriteLine("|| DeviceName      |  " + deviceNameAndOS.Split('_')[0]);
 
             Console.WriteLine("|| AndroidVersion  |  " + deviceNameAndOS.Split('_')[1]);
